7db04f76349cc14e71e2e8ab36ca0a47cb0dbc03
sanneVB <114480939+sanneVB@users.noreply.github.com>
2023-06-13 20:56:58 +0200

    Add skills to index.js
    Skills are inserted into index.html through dom manipulation

--- js/index.js
+++ js/index.js
@@ -1,8 +1,24 @@
-const today = new Date()
-const thisYear = today.getFullYear()
-const footer = document.body.querySelector("footer")
-const copyright = document.createElement("p")
-copyright.innerHTML = `Sanne van Boheemen, &copy;${thisYear}`
-footer.appendChild(copyright)
+// Using DOM manipulation to add information to index.html 
 
-console.log(footer)
+const today = new Date();
+const thisYear = today.getFullYear();
+
+const footer = document.body.querySelector('footer');
+const copyright = document.createElement('p');
+copyright.innerHTML = `Sanne van Boheemen, &copy;${thisYear}`;
+
+footer.appendChild(copyright);
+
+// List skills for the skill section and add them to index.html through DOM manipulation
+const skills = ['HTML', 'CSS', 'JavaScript', 'jQuery', 'SASS', 'Mesh Modeling', 'Solid Modeling', 'FFF 3D Printing', 'SLA 3D Printing' ];
+
+const skillsSection = document.getElementById('skills');
+
+const skillList = skillsSection.querySelector('ul');
+
+for (let i = 0; i < skills.length; i++) {
+  const skill =  document.createElement('li');
+  skill.innerHTML = `${skills[i]}`
+  skillList.appendChild(skill)
+}
+
