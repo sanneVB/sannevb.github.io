be9f4e063eb21b7fea85ca4fc74be3e9d551721b
sanneVB <114480939+sanneVB@users.noreply.github.com>
2023-06-14 23:44:18 +0200

    Take message form input and list them

--- js/index.js
+++ js/index.js
@@ -18,7 +18,25 @@ const skillList = skillsSection.querySelector('ul');
 
 for (let i = 0; i < skills.length; i++) {
   const skill =  document.createElement('li');
-  skill.innerHTML = `${skills[i]}`
-  skillList.appendChild(skill)
+  skill.innerHTML = `${skills[i]}`;
+  skillList.appendChild(skill);
 }
+
+// Message section
+
+const submitMessage = (event) => {
+  const userName = event.target.usersName.value;
+  const userEmail = event.target.usersEmail.value;
+  const userMessage = event.target.usersMessage.value;
+
+  messageList.innerText = `${userName}: ${userMessage}`
+  console.log(`${userName}, ${userEmail}, ${userMessage}`);
+  event.preventDefault();
+}
+
+const messageForm = document.querySelector('form[name=leave_message]');
+const messageSection = document.getElementById('messages')
+const messageList = messageSection.querySelector('ul');
+
+messageForm.addEventListener('submit', submitMessage);
 
