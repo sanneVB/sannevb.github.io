d09481a70c6ec0c99be46ae5064a34261be960b8
sanneVB <114480939+sanneVB@users.noreply.github.com>
2023-06-15 19:22:17 +0200

    Add remove button & hide message section if empty

--- js/index.js
+++ js/index.js
@@ -24,19 +24,50 @@ for (let i = 0; i < skills.length; i++) {
 
 // Message section
 
+// Function that takes the form input, and lists it as a message in the message section
 const submitMessage = (event) => {
   const userName = event.target.usersName.value;
   const userEmail = event.target.usersEmail.value;
   const userMessage = event.target.usersMessage.value;
 
-  messageList.innerText = `${userName}: ${userMessage}`
+  const newMessage =  document.createElement('li');
+  newMessage.innerHTML = `<a href="mailto:${userEmail}">${userName}</a> wrote: ${userMessage}`
+
+  newMessage.appendChild(removeButton);
+  messageList.appendChild(newMessage);
   console.log(`${userName}, ${userEmail}, ${userMessage}`);
   event.preventDefault();
+  event.target.reset();
+  hideMessages();
 }
 
-const messageForm = document.querySelector('form[name=leave_message]');
-const messageSection = document.getElementById('messages')
-const messageList = messageSection.querySelector('ul');
+// Function that removes the parentNode of the event that initiates it
+const removeContent = (event) => {
+  const entry = event.target.parentNode;
+  entry.remove();
+  hideMessages();
+}
 
+// Function that checks for number of li on message section and hides it when 0
+const hideMessages = () => {
+  const messagesList = document.getElementById('messagesList');
+  if (messageList.getElementsByTagName('li').length === 0) {
+    messageSection.style.display = 'none';
+  } else {
+    messageSection.style.display = 'block';
+  }
+}
+
+// Creates and attaches the remove function to any messages
+const removeButton = document.createElement('button');
+removeButton.innerText = 'remove';
+removeButton.setAttribute('type', 'button');
+removeButton.addEventListener('click', removeContent)
+
+// Selects the form and ul elements and runs the submitMessage function when form is submitted
+const messageForm = document.querySelector('form[name=leave_message]');
+const messageSection = document.getElementById('messages');
+const messageList = messageSection.querySelector('ul');
 messageForm.addEventListener('submit', submitMessage);
 
+hideMessages();
